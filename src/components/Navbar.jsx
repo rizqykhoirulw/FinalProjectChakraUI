5b98c2e752c79f74ab9da26e1845a7657657ef0a
Rizqy Khoirul Waritsin <rizqykhoirulw@gmail.com>
2023-06-24 06:58:59 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Box, Button, Text } from "@chakra-ui/react"
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Box, Button, Text, Link, } from "@chakra-ui/react"
 const NavBar = () => {
     return (
         <Box display="flex" justifyContent="space-between" mb="20px" p="20px" bgColor="grey">
-            <Link to={`/`} data-testid="home-page">
+            <Link as={ReactRouterLink} to={`/`} data-testid="home-page">
                 <Text fontSize="22px" fontWeight="semibold" color="white">Student Portal</Text>
             </Link>
             <Box display="flex" justifyContent="space-around" w="25%">
-                <Link to={`/student`}>
+                <Link as={ReactRouterLink} to={`/student`}>
                     <Button data-testid="student-page" bg="transparent" color="white" _hover={{ backgroundColor: "transparent", color: "whiteAlpha.500" }}>All Student</Button>
                 </Link>
-                <Link to={`/add`}>
+                <Link as={ReactRouterLink} to={`/add`}>
                     <Button data-testid="add-page" bg="transparent" color="white" _hover={{ backgroundColor: "transparent", color: "whiteAlpha.500" }}>Add Student</Button>
                 </Link>
             </Box>
